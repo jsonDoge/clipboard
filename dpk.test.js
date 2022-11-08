ab5461893962c251498828a1770e49c105db2baa
jsonDoge <16711523+jsonDoge@users.noreply.github.com>
2022-11-08 16:37:22 +0200

    refactor: adding extra checks for 0

--- dpk.test.js
+++ dpk.test.js
@@ -5,4 +5,22 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+  it("Returns '0' when event paritionKey is string '0'", () => {
+    const event = { partitionKey: '0' };
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toBe("0");
+  });
+
+  it("Returns '0' when event paritionKey is number 0", () => {
+    const event = { partitionKey: 0 };
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toBe("0");
+  });
+
+  it("Returns '1' when event paritionKey is number 1", () => {
+    const event = { partitionKey: 1 };
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toBe("1");
+  });
 });
